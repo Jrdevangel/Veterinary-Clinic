6bff4a5979d9865b6c6d8dbd65787238b79bafcd
anamartin99 <anamariamartinherrera4@gmail.com>
2024-07-22 11:01:21 +0200

    First Commit

---
+++ src/test/java/com/veterinaryclinic/VeterinaryClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veterinaryclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
