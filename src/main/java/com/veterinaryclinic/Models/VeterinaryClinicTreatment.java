6cddc5010da2cdda73055d31b43288b0dc86f107
anamartin99 <anamariamartinherrera4@gmail.com>
2024-07-25 14:57:49 +0200

    Some changes

---
+++ src/main/java/com/veterinaryclinic/Models/VeterinaryClinicTreatment.java
@@ -0,0 +1,48 @@
+package com.veterinaryclinic.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "veterinaryclinic")
+
+public class VeterinaryClinicTreatment {
+
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "name")
+    private String name;
+    @Column(name = "type")
+    private String type;
+    @Column(name = "id")
+    private Long id;
+
+    public void setName(String nameString) {
+        this.name = nameString;
+    }
+
+    public String getName() {
+        return name;
+    
+    }
+
+    public void setType(String typeString) {
+        this.type = typeString;
+    }
+
+    public String getType() {
+        return type;
+    
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    
+    }
+}
