d63e185b0d40f75db709f47b0a76c79f2a455c28
anamartin99 <anamariamartinherrera4@gmail.com>
2024-07-29 15:30:33 +0200

    Some changes

--- src/main/java/com/veterinaryclinic/Services/VeterinaryClinicAppointments.java
+++ src/main/java/com/veterinaryclinic/Services/VeterinaryClinicAppointments.java
@@ -1,5 +1,6 @@
 package com.veterinaryclinic.Services;
 
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.veterinaryclinic.Repositories.IveterinaryClinicAppointmentsRepository;
@@ -9,4 +10,24 @@ public class VeterinaryClinicAppointments {
 
     @Autowired
     IveterinaryClinicAppointmentsRepository veterinaryClinicAppointmentsRepository;
+
+    public ArrayList<VeterinaryClinicAppointments> getAllAppointments() {
+        return veterinaryClinicAppointmentsRepository.findAll();
+    }
+
+    public ArrayList<VeterinaryClinicAppointments> getAppointmentById(Long id) {
+        return veterinaryClinicAppointmentsRepository.findById(id);
+    }
+
+    public VeterinaryClinicAppointments addAppointment(VeterinaryClinicAppointments appointments) {
+        return veterinaryClinicAppointmentsRepository.save();
+    }
+
+    public VeterinaryClinicAppointments updateAppointment(Long id, VeterinaryClinicAppointments updatedAppointments) {
+            return veterinaryClinicAppointmentsRepository.update(id);
+    }
+
+    public void deleteAppointment(Long id) {
+        veterinaryClinicAppointmentsRepository.deleteById(id);
+    }
 }
