e65adcb2effed18fdb886964b11e381be7aceea7
anamartin99 <anamariamartinherrera4@gmail.com>
2024-07-29 12:11:01 +0200

    Some changes

--- src/main/java/com/veterinaryclinic/Services/VeterinaryClinicPatients.java
+++ src/main/java/com/veterinaryclinic/Services/VeterinaryClinicPatients.java
@@ -1,5 +1,14 @@
 package com.veterinaryclinic.Services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.veterinaryclinic.Repositories.IveterinaryClinicPatientsRepository;
+
+@Service
 public class VeterinaryClinicPatients {
+
+    @Autowired
+    IveterinaryClinicPatientsRepository veterinaryClinicPatientsRepository;
     
 }
