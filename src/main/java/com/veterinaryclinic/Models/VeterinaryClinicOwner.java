dc3992fe5c3ecf4521bd2ca4f917df3e7d52a383
anamartin99 <anamariamartinherrera4@gmail.com>
2024-07-30 09:00:36 +0200

    Some changes

--- src/main/java/com/veterinaryclinic/Models/VeterinaryClinicOwner.java
+++ src/main/java/com/veterinaryclinic/Models/VeterinaryClinicOwner.java
@@ -19,6 +19,15 @@ public class VeterinaryClinicOwner {
     @Column(name = "id")
     private Long id;
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+
+    }
+
     public void setOwnerName(String ownerNameString) {
         this.ownername = ownerNameString;
     }
